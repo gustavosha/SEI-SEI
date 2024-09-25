0be279eddae6412976634f0bb468fd412b74c416
gustavosha <116170062+gustavosha@users.noreply.github.com>
2024-09-25 08:59:02 -0300

    Add files via upload

---
+++ Atv quase feito tudo/Aula09_Projeto_Objeto/script.js
@@ -0,0 +1,15 @@
+//criar uma variável do tipo objeto
+
+let pessoa = {
+    nome:'Gustavo',
+    sobrenome:'Shaikoski',
+    idade:'17',
+    altura:'1.66',
+    cargo:'Aluno'
+}
+//consumindo o objeto todo
+console.log(pessoa)
+//consumindo umas das caracteristicas do objeto
+console.log(pessoa.nome)
+console.log(pessoa.altura)
+
